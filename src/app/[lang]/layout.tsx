f7aa41ebcede47923ec83cf6d503fae3d3d256c0
Djordje <djordje007@gmail.com>
2026-02-02 16:09:16 +0100

    chore: debug commit

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import type { LanguageCode } from '@/types/content';
 import AppLayoutContent from '@/components/layout/app-layout-content';
 
-type Params = { lang: LanguageCode };
+type Params = { lang: string };
 type Props = Readonly<{
   children: ReactNode;
   params: Promise<Params>; // KLJUČNO: u Next 15 je Promise
@@ -15,7 +15,7 @@ export default async function LangLayout({ children, params }: Props) {
   // This is a dummy return because the real content is in the RootLayout in app/layout.tsx
   // But we need to await the params here. The child is what matters.
   return (
-      <AppLayoutContent>{children}</AppLayoutContent>
+    <AppLayoutContent>{children}</AppLayoutContent>
   );
 }
 
